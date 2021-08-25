d4160d9ceb6c1cf8ed6301e9dbcdc96d83af43e7
Lucas Viana <lucaasviana280@gmail.com>
2021-08-24 21:31:05 -0300

    bugfix/alterando caminho de referência da pasta view no arquivo app.js, para setar as configurações do template engine

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./src/routes/index');
+var usersRouter = require('./src/routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, './src/views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
