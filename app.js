2a95cf51a2b4bb534cc124001b2cc5475604c1e8
Robert <robertbezerrads@gmail.com>
2021-09-16 19:55:38 -0300

    feature: avançando com o css

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./src/routes/index');
 var usersRouter = require('./src/routes/users');
+var signupRouter = require('./src/routes/signup');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('register', signupRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
