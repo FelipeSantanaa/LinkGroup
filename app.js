2d3f4534673dee244d8a873cdbbdfba5a733446d
Robert <robertbezerrads@gmail.com>
2021-11-11 00:52:16 -0300

    feature: finalizando refatoração

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./src/routes/index');
 var usersRouter = require('./src/routes/users');
 var signupRouter = require('./src/routes/signup');
 var yourInformationRouter = require('./src/routes/your-information');
@@ -21,7 +20,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('register', signupRouter);
 app.use('/your-information', yourInformationRouter);
