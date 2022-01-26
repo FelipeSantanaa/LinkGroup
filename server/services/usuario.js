4193fc9999f96da94a012b74cbb86516680bb4fa
Lucas Viana <lucaasviana280@gmail.com>
2022-01-25 21:33:14 -0300

    feature: alterando arquivos para gerar autenticação de usuário

--- server/services/usuario.js
+++ server/services/usuario.js
@@ -6,4 +6,30 @@ usuarioInterface.createUser = async user => {
   return await Usuario.create({ ...user })
 }
 
+usuarioInterface.getUserByEmail = async email => {
+  const user = await Usuario.findOne({
+    where: email
+  })
+
+  return user
+}
+
+usuarioInterface.getUserById = async id => {
+  const user = await Usuario.findByPk({
+    where: id
+  })
+
+  return user
+}
+
+usuarioInterface.getUserByUsername = async username => {
+  const user = await Usuario.findOne({
+    where: {
+      nome_usuario: username
+    }
+  })
+
+  return user
+}
+
 module.exports = usuarioInterface
--- server/src/controllers/LoginController.js
+++ server/src/controllers/LoginController.js
@@ -1,7 +1,32 @@
+const { getUserById } = require('../../services/usuario')
+
 const LoginController = {
-    index: (req, res, next) => {
-        res.render('login');
+  index: (req, res, next) => {
+    let usuarioLogado = req.cookies.usuario
+    let usuarioAdmin = req.cookies.admin
+
+    console.log(usuarioAdmin)
+    console.log(usuarioLogado)
+
+    if (usuarioLogado || usuarioAdmin) {
+      res.render('admin', {
+        usuarioLogado: req.cookies.usuario,
+        usuarioAdmin: req.cookies.admin
+      })
+    } else {
+      res.render('login')
     }
+  },
+
+  auth: async (req, res, next) => {
+    const user = await getUserById(req.cookies.usuario)
+
+    res.render('admin', {
+      user,
+      usuarioLogado: req.cookies.usuario,
+      usuarioAdmin: req.cookies.admin
+    })
+  }
 }
 
-module.exports = LoginController;
+module.exports = LoginController
