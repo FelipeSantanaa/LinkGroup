2c8d690680fb41cc7876dfcfc360cef6daadc15e
Lucas Viana <lucaasviana280@gmail.com>
2022-01-28 00:02:34 -0300

    bugfix: alterando middleware validadeLogin

--- server/middlewares/validateLogin.js
+++ server/middlewares/validateLogin.js
@@ -1,12 +1,11 @@
 const login = async (req, res, next) => {
-  let { usuario, admin } = req.cookies
-
-  if (!usuario) res.redirect('../login')
-
-  return
-
-  next()
-
+  try {
+    let { usuario, admin } = await req.cookies
+    next()
+  } catch (e) {
+    console.log(e)
+    res.redirect('../login')
+  }
   return
 }
 
