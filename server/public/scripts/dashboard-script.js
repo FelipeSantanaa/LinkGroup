f68a9ece042463f84ce709b00e78fe6a07ed975f
Robert <robertbezerrads@gmail.com>
2022-01-30 10:23:14 -0300

    manipulando o front

--- server/public/scripts/dashboard-script.js
+++ server/public/scripts/dashboard-script.js
@@ -1,7 +1,43 @@
-'use strict'
+// Abre modal
+const openModal = () =>
+  document.getElementById("modal").classList.add("active");
 
-const openModal = () => document.getElementById('modal')
-    .classList.add('active')
+// Fecha modal
+const closeModal = () => {
+  document.getElementById("modal").classList.remove("active");
+  clearFields();
+};
 
-const closeModal = () => document.getElementById('modal')
-    .classList.remove('active')
+// Verificando se os campos do modal estão vazios
+const isValid = () => {
+  return document.getElementById("form").reportValidity();
+};
+
+// Limpando os campos do modal ao fechar
+const clearFields = () => {
+  const fields = document.querySelectorAll(".modal__field");
+  fields.forEach((fields) => (fields.value = ""));
+};
+
+// Capturando e enviando inputs
+const saveUser = () => {
+  if (isValid) {
+    const user = {
+      nome: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+    };
+    console.log(user);
+    closeModal();
+  }
+};
+
+// Função para abrir o modal já com os campos preenchidos
+const fillFields = (user) => {
+    document.getElementById('name').value,
+    document.getElementById('email').value
+}
+
+// Edição o usuário, onde a função fillFields será passada
+const edit = () => {
+    openModal()
+}
