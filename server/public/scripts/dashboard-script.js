01d7bc058b359959b0666896cd9467e9225297d5
Altair Jesus <altair.ajj@gmail.com>
2022-01-31 01:42:38 -0300

    feature: ajustes no modal para edicao do dashboard

--- server/public/scripts/dashboard-script.js
+++ server/public/scripts/dashboard-script.js
@@ -1,35 +1,80 @@
 // Abre modal
 
-const openModal = (nome, email) => {
-  const table = document.querySelector('table')
+const openModal = (nome, email, titulo_perfil, bio, nome_usuario) => {
+  const table = document.querySelector("table");
   const modal = `<div class="modal active" id="modal">
   <div class="modal__content">
     <header class="modal__header">
-      <h2>User</h2>
+      <h2>User Edit</h2>
       <span class="modal__close" id="modalClose" onclick="closeModal()"
         >&#10006;</span
       >
     </header>
     <form id="form" class="modal__form">
-      <input
-        type="text"
-        id="nome"
-        class="modal__field"
-        placeholder="Name"
-        value="${nome}"
-        required
-      />
-      <input
-        type="email"
-        id="email"
-        class="modal__field"
-        placeholder="Email"
-        value="${email}"
-        required
-      />
+    <div class="appearance__container">
+        <form action="/admin/appearance/update" method="POST">
+          <div class="profile__content">
+            <img src="/img/default-profile.png" alt="" />
+            <div>
+            <small>Nome</small>
+              <input
+              type="text"
+              id="name"
+              name="name"
+              class="input__profile"
+              placeholder="Name"
+              value="${nome}"
+              required
+            />
+            <small>E-mail</small>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              class="input__profile"
+              placeholder="Email"
+              value="${email}"
+              required
+            />
+            </div>
+          </div>
+          <small>User Name</small>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            class="input__profile"
+            placeholder="User name"
+            value="${nome_usuario}"
+            required
+          />
+          <small>Profile Title</small>
+            <input
+              class="input__profile"
+              type="text"
+              name="title_profile"
+              id="title_profile"
+              placeholder="Profile Title"
+              value="${titulo_perfil}"
+            />
+            <small>Bio</small>
+            <textarea
+              class="textarea__profile"
+              name="bio"
+              id="bio"
+              cols="30"
+              rows="2"
+              maxlength="80"
+              placeholder="Enter a bio description to appear on your Linktree"
+            >
+${bio}</textarea
+            >
+            <small id="countLength"></small>
+        </div>
+      
       <input type="hidden" name="id" value="${id}">
       <div class="modal__buttons-container">
-        <button id="save" class="btn green" onclick="saveUser()">
+        <button id="save" class="btn purple" onclick="saveUser()">
           Save
         </button>
         <button id="cancel" class="btn red" onclick="closeModal()">
@@ -38,60 +83,60 @@ const openModal = (nome, email) => {
       </div>
     </form>
   </div>
-</div>`
-  table.insertAdjacentHTML('afterend', modal)
-}
+</div>`;
+  table.insertAdjacentHTML("afterend", modal);
+};
 
 // Fecha modal
 const closeModal = () => {
-  document.querySelector('.modal.active').remove()
-  clearFields()
-}
+  document.querySelector(".modal.active").remove();
+  clearFields();
+};
 
 // Verificando se os campos do modal estão vazios
 const isValid = () => {
-  return document.getElementById('form').reportValidity()
-}
+  return document.getElementById("form").reportValidity();
+};
 
 // Limpando os campos do modal ao fechar
 const clearFields = () => {
-  const fields = document.querySelectorAll('.modal__field')
-  fields.forEach(fields => (fields.value = ''))
-}
+  const fields = document.querySelectorAll(".modal__field");
+  fields.forEach((fields) => (fields.value = ""));
+};
 
 // Capturando e enviando inputs
 const saveUser = () => {
   if (isValid) {
     const user = {
-      nome: document.getElementById('name').value,
-      email: document.getElementById('email').value
-    }
-    console.log(user)
-    closeModal()
+      nome: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+    };
+    console.log(user);
+    closeModal();
   }
-}
+};
 
 // Função para abrir o modal já com os campos preenchidos
-const fillFields = user => {
-  document.getElementById('name').value, document.getElementById('email').value
-}
+const fillFields = (user) => {
+  document.getElementById("name").value, document.getElementById("email").value;
+};
 
 // Edição o usuário, onde a função fillFields será passada
 const edit = () => {
-  openModal()
-}
+  openModal();
+};
 
 // Verifica se o usuário é administrador e seta no checkbox
-window.addEventListener('load', function () {
-  let columnsAdmin = document.querySelectorAll('td[id^="admin-user"]')
+window.addEventListener("load", function () {
+  let columnsAdmin = document.querySelectorAll('td[id^="admin-user"]');
   // console.log(columnsAdmin)
 
-  columnsAdmin.forEach(colum => {
-    if (colum.childNodes[1].value == 1) colum.childNodes[3].checked = true
-  })
-})
+  columnsAdmin.forEach((colum) => {
+    if (colum.childNodes[1].value == 1) colum.childNodes[3].checked = true;
+  });
+});
 
 // Excluir linha do usuário selecionado
-const deleteUser = id => {
-  document.getElementById(`line-user-${id}`).remove()
-}
+const deleteUser = (id) => {
+  document.getElementById(`line-user-${id}`).remove();
+};
