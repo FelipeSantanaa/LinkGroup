c21699261d45512071b42b57661c2683b5bd41ee
Altair Jesus <altair.ajj@gmail.com>
2022-01-31 03:19:03 -0300

    feature: acrescentado checkbox Administrador no modal do dashboard

--- server/public/scripts/dashboard-script.js
+++ server/public/scripts/dashboard-script.js
@@ -1,7 +1,15 @@
 // Abre modal
 
-const openModal = (nome, email, titulo_perfil, bio, nome_usuario, id) => {
-  const table = document.querySelector('table')
+const openModal = (
+  nome,
+  email,
+  titulo_perfil,
+  bio,
+  nome_usuario,
+  id,
+  user_admin
+) => {
+  const table = document.querySelector("table");
   const modal = `<div class="modal active" id="modal">
   <div class="modal__content">
     <header class="modal__header">
@@ -37,6 +45,16 @@ const openModal = (nome, email, titulo_perfil, bio, nome_usuario, id) => {
               required
             />
             </div>
+          </div>
+          <div class="admin">
+            <input
+              type="checkbox"
+              id="is_admin"
+              name="admin"
+              value="${admin}"
+              onChange="tornarAdmin()"
+            />
+            <label for="is_admin"><small>Administrador</small></label>
           </div>
           <small>Username</small>
           <input
@@ -83,27 +101,41 @@ ${bio}</textarea
       </div>
     </form>
   </div>
-</div>`
-  table.insertAdjacentHTML('afterend', modal)
-  countLengthBio()
+</div>`;
+  table.insertAdjacentHTML("afterend", modal);
+  countLengthBio();
+
+  const is_admin = document.querySelector("#is_admin");
+  if (user_admin == 1) {
+    is_admin.checked = true;
+  }
+};
+
+// Monitora checkbox Administrador e altera seu value
+const tornarAdmin = () => {
+  if(is_admin.checked) {
+    is_admin.value = 1
+  } else {
+    is_admin.value = 0
+  }
 }
 
 // Fecha modal
 const closeModal = () => {
-  document.querySelector('.modal.active').remove()
-  clearFields()
-}
+  document.querySelector(".modal.active").remove();
+  clearFields();
+};
 
 // Verificando se os campos do modal estão vazios
 const isValid = () => {
-  return document.getElementById('form').reportValidity()
-}
+  return document.getElementById("form").reportValidity();
+};
 
 // Limpando os campos do modal ao fechar
 const clearFields = () => {
-  const fields = document.querySelectorAll('.modal__field')
-  fields.forEach(fields => (fields.value = ''))
-}
+  const fields = document.querySelectorAll(".modal__field");
+  fields.forEach((fields) => (fields.value = ""));
+};
 
 // // Capturando e enviando inputs
 // const saveUser = () => {
@@ -118,37 +150,37 @@ const clearFields = () => {
 // }
 
 // Função para abrir o modal já com os campos preenchidos
-const fillFields = user => {
-  document.getElementById('name').value, document.getElementById('email').value
-}
+const fillFields = (user) => {
+  document.getElementById("name").value, document.getElementById("email").value;
+};
 
 // Edição o usuário, onde a função fillFields será passada
 const edit = () => {
-  openModal()
-}
+  openModal();
+};
 
 // Verifica se o usuário é administrador e seta no checkbox
-window.addEventListener('load', function () {
-  let columnsAdmin = document.querySelectorAll('td[id^="admin-user"]')
+window.addEventListener("load", function () {
+  let columnsAdmin = document.querySelectorAll('td[id^="admin-user"]');
   // console.log(columnsAdmin)
 
-  columnsAdmin.forEach(colum => {
-    if (colum.childNodes[1].value == 1) colum.childNodes[3].checked = true
-  })
-})
+  columnsAdmin.forEach((colum) => {
+    if (colum.childNodes[1].value == 1) colum.childNodes[3].checked = true;
+  });
+});
 
 // Excluir linha do usuário selecionado
-const deleteUser = id => {
-  document.getElementById(`line-user-${id}`).remove()
-}
+const deleteUser = (id) => {
+  document.getElementById(`line-user-${id}`).remove();
+};
 
 function countLengthBio() {
-  const txtBio = document.getElementById('bio'),
-    small = document.getElementById('countLength')
+  const txtBio = document.getElementById("bio"),
+    small = document.getElementById("countLength");
 
-  small.innerText = txtBio.value.length + '/80'
+  small.innerText = txtBio.value.length + "/80";
 
-  txtBio.addEventListener('input', function () {
-    small.innerText = txtBio.value.length + '/80'
-  })
+  txtBio.addEventListener("input", function () {
+    small.innerText = txtBio.value.length + "/80";
+  });
 }
