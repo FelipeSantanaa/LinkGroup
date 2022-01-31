a3f31d827e6318f7dcc70cc1763f7fae629889e7
Altair Jesus <altair.ajj@gmail.com>
2022-01-31 10:48:26 -0300

    hotfix: limpando a main para realizar o merge

--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var IndexController = require('../controllers/IndexController')
-
-/* GET home page. */
-router.get('/', IndexController.index);
-
-module.exports = router;
